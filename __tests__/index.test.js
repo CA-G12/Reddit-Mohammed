7ebfbe570fcc96dd24dea5ce68da10a31a759df3
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-09-03 21:50:18 +0300

    check ci 2

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,7 +6,7 @@ const dbBuild = require('../src/database/config/build');
 describe('Testing 1 return value 1', () => {
   test('Should return 1 when given 1', () => {
     const actual = 1;
-    const expected = 2;
+    const expected = 1;
     expect(actual).toBe(expected);
   });
 });
