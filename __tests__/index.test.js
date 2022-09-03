ebaa011a397bcf696491ad9540a34903cdff4392
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-09-03 21:48:54 +0300

    check CI

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,14 @@
+const connection = require('../src/database/config/connection');
+const dbBuild = require('../src/database/config/build');
+
+// beforeAll(() => dbBuild());
+
 describe('Testing 1 return value 1', () => {
   test('Should return 1 when given 1', () => {
     const actual = 1;
-    const expected = 1;
+    const expected = 2;
     expect(actual).toBe(expected);
   });
 });
+
+// afterAll(() => connection.end());
