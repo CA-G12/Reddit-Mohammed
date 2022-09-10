0762e1135f791b51167238003d3d07ccfae6ddf6
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-09-10 04:07:04 +0300

    upload file feature

--- src/database/config/build.sql
+++ src/database/config/build.sql
@@ -1,6 +1,6 @@
 BEGIN ;
 
-drop table if exists users, posts, comments, communities, user_community, votes cascade;
+drop table if exists users, posts, comments, votes cascade;
 
 create table users (
     id serial primary key,
@@ -8,23 +8,10 @@ create table users (
     email  varchar(255) not null unique, 
     password text  not null,
     about text, 
-    image text default 'https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg'
+    image text default 'https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg',
+    has_Image boolean not null default false
 );
 
-create table communities (
-  id serial primary key,
-  name varchar(21),
-  created_at timestamp not null default CURRENT_TIMESTAMP,
-  about text,
-  image text default 'https://miro.medium.com/max/1200/1*M-b093jQIpmapIIaxH7N7g.jpeg'
-);
-create table user_community(
-  id serial primary key,
-  user_id int,
-  community_id int,
-  foreign key (user_id) references users(id) on delete cascade,
-  foreign key (community_id) references communities(id) on delete cascade
-);
 
 create table posts (
     id serial primary key,
@@ -33,9 +20,7 @@ create table posts (
     image text,
     created_at timestamp not null default CURRENT_TIMESTAMP,
     user_id int not null, 
-    community_id int not null,
-    foreign key (user_id) references users(id) on delete cascade,
-    foreign key (community_id) references communities(id) on delete cascade
+    foreign key (user_id) references users(id) on delete cascade
 
 );
 create table comments (
--- src/database/queries/profile.js
+++ src/database/queries/profile.js
@@ -4,14 +4,22 @@ const getUserPostsQuery = (id) => connection.query(`
 with number_posts as(
     select count(*) from 
     posts where user_id = $1)
-select id,title, content , image, 
-sum((select * from number_posts )) as number_of_posts,
-to_char(created_at,'D-Month-YYYY at hh12-mi- AM')
- from posts where user_id = $1 GROUP by id`, [id]);
+    select id,title, content , image,
+    sum((select * from number_posts )) as number_of_posts,
+    to_char(created_at,'YYYY-MM-DD at HH:MI:SS AM')
+    from posts where user_id = $1 GROUP by id`, [id]);
 
 const getUserQuery = (id) => connection.query(`select * from users 
 where id = $1`, [id]);
 
 const updateBioQuery = (id, bio) => connection.query(`update users
  set about = $1 where id = $2 ;`, [bio, id]);
-module.exports = { getUserPostsQuery, getUserQuery, updateBioQuery };
+
+const updateImageQuery = (id, path) => connection.query(`
+update users set image = $1 , has_image = true where id = $2 returning image`, [path, id]);
+module.exports = {
+  getUserPostsQuery,
+  getUserQuery,
+  updateBioQuery,
+  updateImageQuery,
+};
